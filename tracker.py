ed8d2f41070a4d8fdd2f5087a8d8a10168c82225
jessr0805 <jessesodhi13@gmail.com>
2026-08-11 13:46:02 -0400

    Add single expense input logic and balance tracking

--- tracker.py
+++ tracker.py
@@ -1,3 +1,9 @@
 starting_balance = float(input("What is your starting balance: $"))
 expenses = []
-print(f"Starting balance: ${starting_balance}") 
+print(f"Starting balance: ${starting_balance}")
+input_expensename = str(input("Enter an expense name:"))
+input_expenseprice = float(input("Enter the expense price: $"))
+expenses.append(f"{input_expensename} (${input_expenseprice})")
+balance = starting_balance - input_expenseprice
+print(f"Your current balance is : ${balance}")
+print(f"Your expenses are: {expenses}")
